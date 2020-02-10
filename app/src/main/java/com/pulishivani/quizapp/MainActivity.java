a9d8bb8b3bbdd0c66c529a8135fa011666b24b7d
Shivani <shivanipuli@gmail.com>
2020-02-10 15:18:24 -0500

    committed from personal computer

---
+++ app/src/main/java/com/pulishivani/quizapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.pulishivani.quizapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    public Button button;
+    public EditText responseText;
+    public TextView displayText;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button=findViewById(R.id.clickButton);
+        responseText.findViewById(R.id.responseEditText);
+        displayText.findViewById(R.id.textBox);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                System.out.println("Hello mom");
+                Log.i("displayText", ""+displayText.getText());
+            }
+        });
+        responseText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(!hasFocus){
+                    if(responseText.getText().toString().equals("TJ")){
+                        displayText.setText("TJ Rocks");
+                        responseText.setText("");
+                        responseText.setHint("That's a good name.");
+                    }
+                }
+            }
+        });
+    }
+}
